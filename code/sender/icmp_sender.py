8c551a1803a610011f47ed7398634eeb28c65755
unknown <e270579@metu.edu.tr>
2024-10-30 13:03:55 +0300

    Implement ICMP sender

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,10 @@
-import scapy
+from scapy.all import *
 
-# Implement your ICMP sender here
+def send_icmp_packet():
+    packet = IP(dst="receiver", ttl=1) / ICMP()
+    response = sr1(packet, timeout=1)
+    if response:
+        response.show()
+
+if _name_ == "_main_":
+    send_icmp_packet()
